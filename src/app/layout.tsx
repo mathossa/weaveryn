e4f475c83fa932501dd6d1d5ef34b0eae07ad5dc
Mathijs <mathijs@familie-brand.nl>
2026-08-16 21:32:50 +0200

    Restore minimal footer on latest layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import { AppFooter } from '@/components/app-footer'
 import './globals.css'
 
 const geistSans = Geist({
@@ -29,7 +30,10 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <AppFooter />
+      </body>
     </html>
   )
 }
