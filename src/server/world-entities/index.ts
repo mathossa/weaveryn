50dbc6d5ed40c7a65e54a06a26158948a0dc6710
Mathijs <mathijs@familie-brand.nl>
2026-08-18 02:21:42 +0200

    feat: export World entity input validation

--- src/server/world-entities/index.ts
+++ src/server/world-entities/index.ts
@@ -1,4 +1,5 @@
 export * from './world-entity-errors'
+export * from './world-entity-input'
 export * from './world-entity-repository'
 export * from './world-entity-service'
 export * from './prisma-world-entity-repository'
